b16231cdf68e25eb9e80b529fceba12263d3f386
Mahdi Khaligh <khalighmahdi@users.noreply.github.com>
2026-08-28 11:03:03 +0000

    🔧 Add kotlin-gradle-plugin to buildscript classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.53.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.10")
     }
 }
 
