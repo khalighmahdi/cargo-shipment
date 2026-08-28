600ba5621e0ea29139c1d71c05fcd80b2ac0eb4d
Mahdi Khaligh <khalighmahdi@users.noreply.github.com>
2026-08-28 11:16:00 +0000

    🔧 Add kotlin-allopen to buildscript classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.53.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.10")
+        classpath("org.jetbrains.kotlin:kotlin-allopen:2.2.10")
     }
 }
 
