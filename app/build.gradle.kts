db20f07a2e7f7c648b4f98a3ff64079de6289707
Mahdi Khaligh <khalighmahdi@users.noreply.github.com>
2026-08-28 10:43:31 +0000

    🔧 Use classpath approach for Hilt plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
   alias(libs.plugins.google.devtools.ksp)
-  id("com.google.dagger.hilt.android") version "2.53.1"
+  id("com.google.dagger.hilt.android")
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
   alias(libs.plugins.google.services)
